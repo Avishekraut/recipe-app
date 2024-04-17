39f965c39719c3fe7828b59705f8ce51757893ae
Avishek Raut <avishekraut123@gmail.com>
2024-04-17 06:57:11 +0545

    Initial project setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { RecipeComponent } from './pages/recipe/recipe.component';
+import { RecipeFormComponent } from './pages/recipe-form/recipe-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: RecipeComponent},
+    {path: 'recipe-form', component: RecipeFormComponent},
+    {path: 'recipe-form/:id', component: RecipeComponent},
+
+];
