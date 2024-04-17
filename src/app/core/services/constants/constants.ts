0f92a20a01954bd6d5901dd83dc3fd1fc4f84da3
Avishek Raut <avishekraut123@gmail.com>
2024-04-17 11:29:01 +0545

    add firebase config

---
+++ src/app/core/services/constants/constants.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDp22IDrIp9pJRQN7D1_IpHhTWgh7V8nAM",
+  authDomain: "recipe-app-da515.firebaseapp.com",
+  databaseURL: "https://recipe-app-da515-default-rtdb.firebaseio.com",
+  projectId: "recipe-app-da515",
+  storageBucket: "recipe-app-da515.appspot.com",
+  messagingSenderId: "140969573742",
+  appId: "1:140969573742:web:42c2aba7b59108f868d57f"
+};
